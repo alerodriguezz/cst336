48c4cbf5a465df10373750f0316257c17df5824a
Alex Rodriguez <alerodriguez@csumb.edu>
2017-09-18 08:18:25 +0000

    Landing Page Update

--- index.php
+++ index.php
@@ -2,8 +2,62 @@
     
     <html>
         
-        <h1>Hello World</h1>
+        <head>
+                    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic" rel="stylesheet"> 
+            
+        </head>
         
+        <style >
+            #labs
+            {
+                text-align: left;
+                border: 2px solid black;
+                width:80%;
+                padding-bottom: 5px;
+            }
+            
+            #hw
+            {
+                text-align: left;
+                border: 2px solid black;
+                width:80%;
+            
+            }    
+            
+            tr:nth-child(even) 
+            {
+            background-color:  #dddddd;
+                
+            }
+        </style>
+        
+        <h1 align="center" >Alex Rodriguez</h1>
+        <h2 align="center">CST 336</h2>
+        
+        <table id="labs" align="center">
+            <tr>
+                <th>Labs</th>
+                <th>Link</th>
+            </tr>
+            <tr>
+                <td>Lab 1</td>
+                <td> <a target="_blank" href="https://cst336-alerodriguez.herokuapp.com/PersonalPortfolio/index.html">https://cst336-alerodriguez.herokuapp.com/PersonalPortfolio/index.html</a></a></td>
+            </tr>
+            <tr>
+                <td>Lab 2</td>
+            </tr>
+        </table>
+        <br>
+        <table id="hw" align="center">
+            <tr>
+                <th>HW</th>
+                <th>Link</th>
+            </tr>
+              <tr>
+                <td>HW 1</td>
+                <td> <a target="_blank" href="https://cst336-alerodriguez.herokuapp.com/HW/hw1/index.html">https://cst336-alerodriguez.herokuapp.com/HW/hw1/index.html</a></td>
+            </tr>
+        </table>
         
         
         
