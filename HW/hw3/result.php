9a1a6c4cd97be04fa7a14e45f0b7cc01e1eabe3f
Alex Rodriguez <alerodriguez@csumb.edu>
2017-10-07 06:56:14 +0000

    hw 3 submit

--- HW/hw3/result.php
+++ HW/hw3/result.php
@@ -1,3 +1,5 @@
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; 
+Welcome <?php echo $_POST['name']; ?><br>
+Your email address is: <?php echo $_POST["email"];
+
+ <button type="button" onclick="index.html">Click Me!</button> 
