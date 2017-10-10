ed33ec3b7f66e63e4f570ea78d81085c0eaa4f5a
Alex Rodriguez <alerodriguez@csumb.edu>
2017-10-10 08:26:22 +0000

    lab 2 update

--- Labs/lab2/lab2.php
+++ Labs/lab2/lab2.php
@@ -1,2 +1,17 @@
+<?php
 
- echo '<img src="images/cat.jpg">'; 
+ $n = rand(0,3);  
+
+ switch ($n) {
+    case 0:
+        echo '<img src="images/cat.jpg">';
+        break;
+    case 1:
+        echo '<img src="images/frog.jpg">';
+        break;
+    case 2:
+        echo '<img src="images/sloth.jpg">';
+        break;
+    case 3:
+        echo '<img src="images/wiener_dog.jpg">';
+}
