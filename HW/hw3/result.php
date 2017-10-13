af120a3844266be0fb2c208cea8eb58a6b45c461
Alex Rodriguez <alerodriguez@csumb.edu>
2017-10-13 11:37:32 +0000

    misc update (ignore for grading)

--- HW/hw3/result.php
+++ HW/hw3/result.php
@@ -2,4 +2,7 @@
 Welcome: <?php echo $_POST['name']; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?> <br> 
 
-"Your information has been processed...";
+Your information has been processed...
+
+<form action="retry.php"><input type="submit" value="Try again" /></form>
+
