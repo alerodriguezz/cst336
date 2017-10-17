b6892463d776e59f1bbf63ee55211265ef1812a7
Alex Rodriguez <alerodriguez@csumb.edu>
2017-10-17 20:58:53 +0000

    lab 5 init

---
+++ Labs/lab5/index.php
@@ -0,0 +1,27 @@
+<?php
+//Step1
+ $db = mysqli_connect('localhost','username','password','database_name')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+    <head>
+        <title>Lab 5 </title>
+    </head>
+    <body>
+        <div align="center">
+            <h1 >
+                lab5_list
+            </h1>
+        </div>
+        
+        
+        <table>
+            
+            
+            
+            
+        </table>
+
+    </body>
+</html>
