d6fb242492a4425843fca1b8f79579501440b2a0
Alex Rodriguez <alerodriguez@csumb.edu>
2017-11-29 23:16:34 +0000

    Lab 7: Fill-In works now for longitude and latitude, but only sometimes for city

--- Labs/lab6/info.php
+++ Labs/lab6/info.php
@@ -22,11 +22,12 @@ $result = $dbConn->query($display_sql);
 
 <html lang="en">
     <head>
+              <meta charset="utf-8">
         <title>
             Info
         </title>
           <link href="css/styles.css" rel="stylesheet" type="text/css"/>
-          <meta charset="utf-8">
+        
           
             <meta name="viewport" content="width=device-width, initial-scale=1">
 
