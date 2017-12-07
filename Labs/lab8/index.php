35d78ee4a375093777d7047056982811ed339772
Alex Rodriguez <alerodriguez@csumb.edu>
2017-12-07 00:57:03 +0000

    lab8 in progres home page up

---
+++ Labs/lab8/index.php
@@ -0,0 +1,94 @@
+<html>
+    <head>
+        <title>
+            Lab 8
+        </title>
+        
+         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+           <meta charset="utf-8">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <!--Navigation Bar Code-->
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#news">Adoptions</a></li>
+          <li><a href="#contact">About Us</a></li>
+        </ul>
+        
+        <!--Text-->
+        <center>
+        <h1>CSUMB Animal Shelter</h1>
+        The official adoption website of CSUMB
+        
+        <!-- Carousel -->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <img src="img/alex.jpg" alt="Alex">
+            </div>
+        
+            <div class="item">
+              <img src="img/bear.jpg" alt="Bear">
+            </div>
+        
+            <div class="item">
+              <img src="img/carl.jpg" alt="Carl">
+            </div>
+            
+             <div class="item">
+              <img src="img/charlie.jpg" alt="Charlie">
+            </div>
+            
+             <div class="item">
+              <img src="img/lion.jpg" alt="Lion">
+            </div>
+            
+             <div class="item">
+              <img src="img/otter.jpg" alt="Otter">
+            </div>
+            
+             <div class="item">
+              <img src="img/sally.jpg" alt="Sally">
+            </div>
+            
+             <div class="item">
+              <img src="img/samantha.jpg" alt="Samantha">
+            </div>
+            
+             <div class="item">
+              <img src="img/ted.jpg" alt="Ted">
+            </div>
+            
+             <div class="item">
+              <img src="img/tiger.jpg" alt="Tiger">
+            </div>
+            
+          </div>
+        
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        
+        <a href="adoptions.php" ><button class="button">
+            Adopt Now!
+        </button></a>
+        
+        </center>
+        
+        
+        
+    </body>
+</html>
