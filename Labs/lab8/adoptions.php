0f8028434cba951c1762b71430bf7b4ce8a633f0
Alex Rodriguez <alerodriguez@csumb.edu>
2017-12-11 10:14:18 +0000

    officially got lab 8 functioning dynamically

--- Labs/lab8/adoptions.php
+++ Labs/lab8/adoptions.php
@@ -50,7 +50,13 @@ $result = $dbConn->query($sql);
                 	<tr cellpadding="10">
                 		<td class="animal"><?php echo "Name: ".$row['name'];?><br>
                 		    <?php  echo "Type: ". $row['type']; ?>
-                		    <button type="button" class="btn btn-primary btn-lg" class="button" id="<?php echo $row['name'] ?>" data-toggle="modal" data-target="#myModal"> Adopt Me!</button>
+                		    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" 
+                		            data-name="<?php echo $row['name']; ?>" 
+                		            data-pictureurl="<?php echo $row['pictureURL']; ?>"
+                		            data-yob="<?php echo $row['yob']; ?>"
+                		            data-breed="<?php echo $row['breed']; ?>"
+                		            data-description="<?php echo $row['description']; ?>"
+                		            > Adopt Me!</button>
                 		    	 <?php }?>
                 		      </td>
                 	</tr>
@@ -66,8 +72,13 @@ $result = $dbConn->query($sql);
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title" id="myModalLabel">Modal title</h4>
                   </div>
-                  <div class="modal-body">
-                    ...
+                  <div class="modal-body" style="float: left">
+                      <img src="" class="showPic" >
+                          <div id="popupLayout" style="float: left">
+                              <p id="age"></p><br>
+                              <p id="breed"></p><br>
+                              <p id="description"></p><br>
+                          </div>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -81,5 +92,30 @@ $result = $dbConn->query($sql);
 </html>
 
 <script>
+//retrieve data based on button id 
+//query database 
+
+//display it in modal
+
+$(document).on("click", ".btn", function () {
+    //retrieve data from database
+    console.log("popup function started");
+     var name = $(this).data('name'); 
+     var picURL= $(this).data('pictureurl');
+     var yob= $(this).data('yob');
+     var breed= $(this).data('breed');
+     var descr= $(this).data('description');
+     //calculate current age 
+     var age= new Date().getFullYear()-yob;
+    console.log(name+" "+picURL+ " "+age+ " "+ breed +" "+ descr);
+     $(".modal-header h4").html( name );
+     $('.modal-body .showPic').attr('src', 'img/'+picURL);
+     $(".modal-body #age").html("Age: "+ age );
+     $(".modal-body #breed").html("Breed:  "+ breed );
+      $(".modal-body #description").html("Description:  "+ descr );
+     
+});
+
+    
 
 </script>
