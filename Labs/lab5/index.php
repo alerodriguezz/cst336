a40f8cbb0804695300d26f58be242e3993daee5d
Alex Rodriguez <alerodriguez@csumb.edu>
2017-12-16 20:35:15 +0000

    lab 5 filter feature almost done

--- Labs/lab5/index.php
+++ Labs/lab5/index.php
@@ -1,8 +1,4 @@
-
-
 <?php
-
-
 //env variables
 $USERNAME= getenv('USERNAME');
 $PASSWORD= getenv('PASSWORD');
@@ -13,17 +9,37 @@ $dbConn = new PDO("mysql:host=localhost;dbname=tech_checkout", $USERNAME, $PASSW
 // Setting Errorhandling to Exception
 $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
-
 //write sql query and assign to variable 
+//default query
 $sql = "SELECT * FROM device" ;
+
+//type query
+$type_query="SELECT * FROM device
+ORDER BY deviceType;";
+
+//name query 
+
+
+//availability query
+
+
+//price query
+
+
 $result = $dbConn->query($sql);
 
 ?>
-
+<html>
 <head>
     <title>
         lab5
     </title>
+         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+           <meta charset="utf-8">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <h1 align="center">
@@ -31,17 +47,31 @@ $result = $dbConn->query($sql);
 </h1>
 
 <body>
-    
-            
+     <div align="center">
+      <form method="post" align="center">
+          Filter by: 
+          	   <select name="filter" id="filter">
+          	      <option value=""> None </option>
+          	      <option value="deviceId"> ID </option>
+          	   	  <option value="deviceName"> Name </option>
+          	   	  <option value="deviceType"> Type </option>
+          	   	  <option value="price"> Price </option>
+          	   	  <option value="status"> Status </option>
+          	   </select>
+         
+         </form>
+        </div>
             <div align="center">
                 <table border="1" cellspacing="0" cellpadding="2" >
-                <thead>
-                	<tr>
-                		<th> Device ID </th>
-                		<th> Device Name </th>
-                		<th> Device Type </th>
-                	</tr>
-                </thead>
+                    <thead>
+                    	<tr>
+                    		<th> Device ID </th>
+                    		<th> Device Name </th>
+                    		<th> Device Type </th></button>
+                    		<th> Price </th>
+                        	<th> Status </th>
+                    	</tr>
+                    </thead>
                 <tbody>
                
                <?php
@@ -51,6 +81,9 @@ $result = $dbConn->query($sql);
                 		<td><?php echo $row['deviceId']; ?></td>
                 		<td><?php echo $row['deviceName']; ?></td>
                 		<td><?php echo $row['deviceType']; ?></td>
+                		<td><?php echo $row['price']; ?></td>
+                		<td><?php echo $row['status']; ?></td>
+                		
                 	</tr>
                 	<?php
                 		}
@@ -59,3 +92,49 @@ $result = $dbConn->query($sql);
                 </table>
             </div>
 </body>
+
+<div id="show">
+  <!-- ITEMS TO BE DISPLAYED HERE -->
+</div>
+
+</html>
+
+<script>
+   
+     $(document).ready(function(){ /* PREPARE THE SCRIPT */
+    $("#filter").change(function(){
+        console.log("selected option...");/* WHEN YOU CHANGE AND SELECT FROM THE SELECT FIELD */
+      var query = $(this).val(); /* GET THE VALUE OF THE SELECTED DATA */
+      var dataString = "filter="+query; /* STORE THAT TO A DATA STRING */
+            console.log(dataString);
+      $.ajax({ /* THEN THE AJAX CALL */
+        type: "POST", /* TYPE OF METHOD TO USE TO PASS THE DATA */
+        url: "index.php", /* PAGE WHERE WE WILL PASS THE DATA */
+        data: dataString, /* THE DATA WE WILL BE PASSING */
+        success: function(result){ /* GET THE TO BE RETURNED DATA */
+          //$("#show").html(result); /* THE RETURNED DATA WILL BE SHOWN IN THIS DIV */
+            window.location.href="index.php?"+dataString;
+        }
+      });
+
+    });
+  });
+  
+      function getQueryVariable(variable)
+                {
+                       var query = window.location.search.substring(1);
+                       var vars = query.split("&");
+                       for (var i=0;i<vars.length;i++) {
+                               var pair = vars[i].split("=");
+                               if(pair[0] == variable){return pair[1];}
+                       }
+                       return(false);
+                }
+  
+  if (getQueryVariable("filter")!=false)
+  {   var pizza= getQueryVariable("filter");
+      console.log(pizza + " has been recieved.");
+  }
+  
+  
+</script>
