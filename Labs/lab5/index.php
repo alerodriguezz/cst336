ada19ab9217c183dd3eeaf435b85ddf906c78a15
Alex Rodriguez <alerodriguez@csumb.edu>
2017-12-18 06:16:04 +0000

    lab 7 update

--- Labs/lab5/index.php
+++ Labs/lab5/index.php
@@ -130,22 +130,6 @@ $result = $dbConn->query($sql);
     });
   });    
   
-//       function getQueryVariable(variable)
-//                 {
-//                       var query = window.location.search.substring(1);
-//                       var vars = query.split("&");
-//                       for (var i=0;i<vars.length;i++) {
-//                               var pair = vars[i].split("=");
-//                               if(pair[0] == variable){return pair[1];}
-//                       }
-//                       return(false);
-//                 }
-  
-//   if (getQueryVariable("filter")!=false)
-//   {   var pizza= getQueryVariable("filter");
-//       console.log(pizza + " has been recieved.");
-//       document.cookie = "filter="+pizza;
-//   }
   
   
 </script>
